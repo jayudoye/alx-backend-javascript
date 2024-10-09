b3668c74b256acbb560c65c65bdceb93cb78b56a
jayudoye <joeludoye@gmail.com>
2024-10-09 04:10:57 +0100

    ES6 Data Manipulation

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+export default function cleanSet(set, startString) {
+  // Filter set values that start with the specified string
+  const filteredValues = Array.from(set).filter((value) => typeof value === 'string' && value.startsWith(startString));
+
+  // Join the filtered values into a single string separated by '-'
+  const res = filteredValues.map((obj) => obj.slice(startString.length));
+  return typeof startString === 'string' && startString.length ? res.join('-') : '';
+}
