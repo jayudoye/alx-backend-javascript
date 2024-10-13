0ddc90b9a02170ecc59b04b6fca9dea933fd3133
jayudoye <joeludoye@gmail.com>
2024-10-13 23:51:09 +0100

    ES6 basic

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
+    // eslint disabled line of code
     const value = array.indexOf(idx);
     array[value] = appendString + idx;
   }
